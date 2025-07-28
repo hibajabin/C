d6e2b74b56018b0692255a829ec05a5f5b6251bb
hibajabin <hibajabinkdy@gmail.com>
2025-07-27 22:47:00 -0700

    Add files via upload

---
+++ reverse left half pyramid.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k;
+    for(i=0;i<=5;i++){
+        for(j=0;j<i;j++){
+            printf("  ");
+        }
+        for(k=0;k<5-i;k++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
